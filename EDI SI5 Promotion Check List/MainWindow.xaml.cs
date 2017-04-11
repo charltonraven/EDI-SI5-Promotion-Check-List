b7839e6a4364b38cf79849af6b2a68f748634c5b
charltonraven <williamscharlton@hotmail.com>
2017-04-11 14:41:01 -0400

    default start

--- EDI SI5 Promotion Check List/MainWindow.xaml.cs
+++ EDI SI5 Promotion Check List/MainWindow.xaml.cs
@@ -19,7 +19,45 @@ namespace EDI_SI5_Promotion_Check_List
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+
     {
+        String User;
+        String Partner;
+        String Date;
+        String CMRN;
+        bool UAOP;
+        bool PAOIP;
+        bool tableParm;
+        String tableParmName;
+        bool developementCompleted;
+        bool testingCompleted;
+        bool codeReview;
+        String codeReviewBY;
+        String codeReviewDate;
+        bool keyUserSignOff;
+        bool partnerSignOff;
+        String impFinalStatus;
+        String PostImpReview;
+
+        bool Envelopes;
+        bool BP;
+        bool ServiceAdapters;
+        bool perlScripts;
+        bool EmailCodeList;
+        bool docMaps;
+        bool docExtractionMap;
+        bool XSLTEmail;
+        bool mapCodeTables;
+        bool RAILStable;
+        bool RAILSrecord;
+        bool RAILSfilter;
+        bool fileStructureProd;
+        bool FTPconnect;
+        bool TRANSPORTfile;
+
+        String ProjectManager;
+        String CompletionDate;
+        String to;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,43 +66,7 @@ namespace EDI_SI5_Promotion_Check_List
         private void btnSend_Click(object sender, RoutedEventArgs e)
         {
             
-            String User;
-            String Partner;
-            String Date;
-            String CMRN;
-            bool UAOP;
-            bool PAOIP;
-            bool tableParm;
-            String tableParmName;
-            bool developementCompleted;
-            bool testingCompleted;
-            bool codeReview;
-            String codeReviewBY;
-            String codeReviewDate;
-            bool keyUserSignOff;
-            bool partnerSignOff;
-            String impFinalStatus;
-            String PostImpReview;
-
-            bool Envelopes;
-            bool BP;
-            bool ServiceAdapters;
-            bool perlScripts;
-            bool EmailCodeList;
-            bool docMaps;
-            bool docExtractionMap;
-            bool XSLTEmail;
-            bool mapCodeTables;
-            bool RAILStable;
-            bool RAILSrecord;
-            bool RAILSfilter;
-            bool fileStructureProd;
-            bool FTPconnect;
-            bool TRANSPORTfile;
-
-            String ProjectManager;
-            String CompletionDate;
-            String to;
+        
 
         }
     }
