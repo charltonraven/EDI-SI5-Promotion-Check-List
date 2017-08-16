8a758c6e31e1734dd8f9523579f24018cf0f76ff
charltonraven <williamscharlton@hotmail.com>
2017-08-15 20:25:20 -0400

    Fixed Pop up to Load in the middle

--- EDI SI5 Promotion Check List/ProjectManager_Name.xaml.cs
+++ EDI SI5 Promotion Check List/ProjectManager_Name.xaml.cs
@@ -44,5 +44,13 @@ namespace EDI_SI5_Promotion_Check_List
             
             this.Close();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Application curApp = Application.Current;
+            Window mainWindow = curApp.MainWindow;
+            this.Left = mainWindow.Left + (mainWindow.Width - this.ActualWidth) / 2;
+            this.Top = mainWindow.Top + (mainWindow.Height - this.ActualHeight) / 2;
+        }
     }
 }
